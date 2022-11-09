758b28abd96177f8d62f3e05ed3f7da8fe7455cc
Obriel Zvoushe <obrielzvoushe1@gmail.com>
2022-11-09 21:58:48 +0200

    Create UserRepository

---
+++ src/main/java/com/obrielz/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.obrielz.repository;
+
+import com.obrielz.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
